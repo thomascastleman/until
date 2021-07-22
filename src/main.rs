77bc67e73572e6f3b0a1680daa6e673b7e3ff1a6
Thomas Castleman <thomascastleman0@gmail.com>
2021-07-22 18:32:54 -0400

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,46 @@
-use chrono::{Duration, Local, TimeZone};
+use chrono::{Local, TimeZone};
+use duration_breakdown::DurationBreakdown;
 use std::{
-    env, fmt,
+    env,
+    error::Error,
     io::{self, Write},
-    process, thread,
+    thread,
 };
 
 /// The format in which dates should be supplied to the program.
 /// See [chrono's docs](https://docs.rs/chrono/0.4.19/chrono/format/strftime/index.html)
-const DATE_FORMAT: &'static str = "%Y-%m-%d %T";
+const DATE_FORMAT: &str = "%Y-%m-%d %T";
 
+/// Prints a message (without a linebreak) and then flushes stdout immediately.
 macro_rules! print_and_flush {
     ($fmt:expr, $($args:tt)*) => {
-        print!("{}", std::format_args!($fmt, $($args)*));
+        print!($fmt, $($args)*);
         io::stdout().flush().unwrap();
     };
 }
 
 fn main() {
-    let date = env::args().nth(1).unwrap_or_else(|| {
-        println!("Expected a date in the format: {}", DATE_FORMAT);
-        process::exit(1);
-    });
+    if let Err(e) = run() {
+        eprintln!("error: {}", e);
+        std::process::exit(-1);
+    }
+}
 
-    let event = Local
-        .datetime_from_str(&date, DATE_FORMAT)
-        .unwrap_or_else(|err| {
-            println!("Error parsing date: {}", err);
-            println!("Expecting format: {}", DATE_FORMAT);
-            process::exit(1);
-        });
+fn run() -> Result<(), Box<dyn Error>> {
+    let date = env::args()
+        .nth(1)
+        .ok_or(format!("expected a date in the format: {}", DATE_FORMAT))?;
+
+    let event = Local.datetime_from_str(&date, DATE_FORMAT)?;
 
     loop {
-        // compute time until event as a partitioned duration
-        let until = PartitionedDuration::new(event - Local::now());
+        // compute time until event as a duration breakdown
+        let duration = (event - Local::now()).to_std()?;
+        let until = DurationBreakdown::from(duration);
 
         // display duration, wait, then erase line
-        print_and_flush!("{}", until);
+        print_and_flush!("{}", until.as_string_hide_zeros());
         thread::sleep(std::time::Duration::from_millis(500));
         print_and_flush!("{}", "\x1b[2K\x1b[1G"); // erase line
-    }
-}
-
-/// PartitionedDuration represents a Duration, split into component parts in such
-/// a way that the sum of the parts (despite their different units) would
-/// equal the whole duration.
-struct PartitionedDuration {
-    weeks: i64,
-    days: i64,
-    hours: i64,
-    minutes: i64,
-    seconds: i64,
-}
-
-impl PartitionedDuration {
-    /// Constructs a new PartitionedDuration from a normal chrono::Duration,
-    /// by splitting it into its component parts (weeks, days, hours, ...)
-    pub fn new(mut duration: Duration) -> Self {
-        // extract weeks
-        let weeks = duration.num_weeks();
-        duration = duration - Duration::weeks(weeks);
-
-        // extract days
-        let days = duration.num_days();
-        duration = duration - Duration::days(days);
-
-        // extract hours
-        let hours = duration.num_hours();
-        duration = duration - Duration::hours(hours);
-
-        // extract minutes
-        let minutes = duration.num_minutes();
-        duration = duration - Duration::minutes(minutes);
-
-        // extract seconds
-        let seconds = duration.num_seconds();
-
-        PartitionedDuration {
-            weeks,
-            days,
-            hours,
-            minutes,
-            seconds,
-        }
-    }
-}
-
-impl fmt::Display for PartitionedDuration {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fn plural(quantity: i64) -> String {
-            (if quantity == 1 { "" } else { "s" }).to_string()
-        }
-
-        write!(
-            f,
-            "{} week{}, {} day{}, {} hour{}, {} minute{}, and {} second{}",
-            self.weeks,
-            plural(self.weeks),
-            self.days,
-            plural(self.days),
-            self.hours,
-            plural(self.hours),
-            self.minutes,
-            plural(self.minutes),
-            self.seconds,
-            plural(self.seconds)
-        )
     }
 }
